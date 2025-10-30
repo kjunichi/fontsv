e2d4f2aa1721c43121f44f32e78c49718566ccca
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2025-10-30 13:07:57 +0900

    test:  refactoring tests

--- kanji.test.js
+++ kanji.test.js
@@ -4,44 +4,17 @@ const assert = require('node:assert')
 const kanji = require('./kanji')
 const testClient = require('hono/testing').testClient
 
-if (require.main === module) {
-  main()
-}
 
-async function main () {
-  try {
-    const tests = [ // <2>
-      helloTest,
-      alwaysFailTest,
-    ]
-
-    for (const test of tests) { // <3>
-      try {
-        await test()
-        console.info(`OK ${test.name}`) // <4>
-      } catch (err) {
-        console.info(`NG ${test.name}`) // <5>
-        console.error(err)
-      }
-    }
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-async function helloTest () {
+test('kanji', async (context) => {
     const client = testClient(kanji)
 
     const res = await client.moji.$get({
-      query: { m: 'hono' },
+      query: { m: 'A' }
     })
 
-  
     assert.strictEqual(res.status, 200)
-}
-
-async function alwaysFailTest () {
-  assert.fail('alwaysFail')
-}
+    const json = await res.json()
+    assert.deepStrictEqual(json, [ {frame: [ 16394,10489344,2685009920]}])
+})
 
 
